81d3bec42563517a706c97b4756429e62a1fec92
PixelJanitor <derek.briggs@me.com>
2019-07-01 21:55:10 -0400

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    foo
+    <div class="temp-header"></div>
   </div>
 </template>
 
@@ -9,3 +9,11 @@ export default {
   components: {}
 }
 </script>
+
+<style>
+.temp-header {
+  height: 625px;
+  /* background: url(pattern@2x.png), radial-gradient(1128.82px at 0% 100%, #FF7F50 0%, #FF6B81 100%); */
+  background: radial-gradient(1128.82px at 0% 100%, #ff7f50 0%, #ff6b81 100%);
+}
+</style>
