3e6dd71abb11db636b55ea1430c6356392814168
PixelJanitor <derek.briggs@me.com>
2019-07-30 15:38:35 -0400

    tailwind config wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,59 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
-    extend: {}
+    fontSize: {
+      'xs': '10px',
+      'sm': '12px',
+      'base': '14px',
+      'lg': '16px',
+      'xl': '18px',
+      '2xl': '20px',
+    },
+    borderRadius: {
+      'none': '0',
+      'px': '1px',
+      'sm': '2px',
+      default: '4px',
+      'lg': '8px',
+      'xl': '16px',
+    },
+    extend: {
+      padding: {
+        px: '1px',
+        xs: '2px',
+        sm: '4px',
+        '0': '0',
+        '1': '8px',
+        '1-1/2': '12px',
+        '2': '16px',
+        '3': '24px',
+        '4': '32px',
+        '5': '40px',
+        '6': '48px',
+        '7': '56px',
+        '8': '64px',
+        '9': '72px',
+        '16': '128px',
+      },
+      margin: {
+        auto: 'auto',
+        px: '1px',
+        xs: '2px',
+        sm: '4px',
+        '0': '0',
+        '1': '8px',
+        '1-1/2': '12px',
+        '2': '16px',
+        '3': '24px',
+        '4': '32px',
+        '5': '40px',
+        '6': '48px',
+        '7': '56px',
+        '8': '64px',
+        '9': '72px',
+        '16': '128px',
+      },
+    },
   },
   variants: {}
 }
