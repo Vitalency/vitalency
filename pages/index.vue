f51afb7863c2561bd4d5b17fdc5db8e79c3d1c73
PixelJanitor <derek.briggs@me.com>
2019-07-30 15:38:43 -0400

    home page wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <div class="temp-header" />
+  <div class="flex">
+    <div class="hero">
+      <header class="p-4">
+        <img src="~assets/images/logo-full-white.svg" alt="vitalency logo" />
+      </header>
+      <div class="pl-8">
+        <div class="hero-content">
+          <div>
+            Check-ins to keep you in check
+          </div>
+          <div class="signup-form">Join Vitalency</div>
+        </div>
+      </div>
+    </div>
+    <div class="hero-sidebar">
+      <header class="flex justify-end p-4">
+        <div class="btn">
+          SIGN IN
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
@@ -11,9 +30,56 @@ export default {
 </script>
 
 <style>
-.temp-header {
-  height: 625px;
-  /* background: url(pattern@2x.png), radial-gradient(1128.82px at 0% 100%, #FF7F50 0%, #FF6B81 100%); */
+.hero {
+  @apply relative;
+  height: 100vh;
+  flex: 3;
   background: radial-gradient(1128.82px at 0% 100%, #ff7f50 0%, #ff6b81 100%);
+}
+
+.hero::after {
+  content: '';
+  @apply absolute;
+  @apply top-0;
+  @apply left-0;
+  @apply w-full;
+  @apply h-full;
+  opacity: 0.08;
+  background: url('~assets/images/pattern-white.svg');
+}
+
+.hero-sidebar {
+  @apply bg-white;
+  @apply flex-1;
+}
+
+.hero-content {
+  @apply flex;
+  @apply items-center;
+  @apply justify-between;
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  background: gray;
+}
+
+.signup-form {
+  @apply bg-white;
+  @apply p-4;
+  @apply border;
+  @apply rounded;
+  width: 50%;
+  max-width: 368px;
+  transform: translateX(50%);
+}
+
+.btn {
+  @apply px-2;
+  @apply py-1;
+  font-size: 12px;
+  height: 42px;
+  background: rgba(255, 107, 129, 0.04);
+  border: 1px solid rgba(255, 107, 129, 0.4);
+  border-radius: 21px;
 }
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,44 +1,64 @@
 <template>
   <div class="w-full max-w-xs">
-    <form @submit.prevent="login" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      @submit.prevent="login"
+    >
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email address</label>
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+          Email address
+        </label>
         <input
           id="email"
+          v-model="email"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           placeholder="Email"
-          v-model="email"
         />
       </div>
       <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="password"
+        >
+          Password
+        </label>
         <input
           id="password"
+          v-model="password"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           type="password"
           placeholder="******************"
-          v-model="password"
         />
-        <p class="text-xs italic">Please choose a password.</p>
+        <p class="text-xs italic">
+          Please choose a password.
+        </p>
       </div>
       <div class="flex items-center justify-between">
         <button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
-        >Sign In</button>
+        >
+          Sign In
+        </button>
         <a
           class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
           href="#"
-        >Forgot Password?</a>
+        >
+          Forgot Password?
+        </a>
       </div>
     </form>
 
-    <form @submit.prevent="loginWithFacebook" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      @submit.prevent="loginWithFacebook"
+    >
       <div class="flex items-center justify-between">
         <button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit">
+          type="submit"
+        >
           Sign In With Facebook
         </button>
       </div>
@@ -58,30 +78,30 @@ export default {
     }
   },
   methods: {
-    login: function () {
+    login: function() {
       firebase
         .auth()
         .signInWithEmailAndPassword(this.email, this.password)
         .then(
-          function (user) {
+          function(user) {
             alert('Well done!')
           },
-          function (error) {
+          function(error) {
             alert('Ooops! ' + error.message)
           }
         )
     },
-    loginWithFacebook: function () {
+    loginWithFacebook: function() {
       this.provider = new firebase.auth.FacebookAuthProvider()
-      firebase.auth().signInWithPopup(this.provider)
-        .then(
-          (result) => {
-            this.$router.push('/')
-          })
-        .catch(
-          (error) => {
-            alert('!Ooops! ' + error.message)
-          })
+      firebase
+        .auth()
+        .signInWithPopup(this.provider)
+        .then((result) => {
+          this.$router.push('/')
+        })
+        .catch((error) => {
+          alert('!Ooops! ' + error.message)
+        })
     }
   }
 }
